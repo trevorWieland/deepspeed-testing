c057a80a08436c0d804dca0a316260bc6eb644f5
Volundr AI <trevor_wieland@mac.com>
2022-06-29 14:51:45 +0900

    Functioning

--- containers/deepspeedrunpod/start.sh
+++ containers/deepspeedrunpod/start.sh
@@ -15,5 +15,13 @@ fi
 
 mkdir /workspace
 cd /workspace
-git clone https://github.com/huggingface/transformers.git
 git clone https://github.com/trevorWieland/deepspeed-testing.git
+git clone https://github.com/huggingface/transformers.git
+
+if [[ $JUPYTER_PASSWORD ]]
+then
+    cd /
+    jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
+else
+    sleep infinity
+fi
